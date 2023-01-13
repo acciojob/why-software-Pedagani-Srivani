e66cc8bf0514b1180b1cf148b9609d8b2e21a72c
Pedagani-Srivani <117532392+Pedagani-Srivani@users.noreply.github.com>
2023-01-13 12:44:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your code here
+<html>
+
+<head>
+<link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+<!-- the entire body must be written by student -->
+<h1>Why I want to become a developer?</h1>
+<p>to develop new technologies</p>
+<script type="text/javascript" src="./script.js">
+</script>
+
+</body>
+
+</html>
